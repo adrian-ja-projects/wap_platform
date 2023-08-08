a3431f529cf694e4e27b104aae90eb2fba19f64d
adrian-ja-projects <adrian.jimeneza@gmail.com>
2023-08-08 19:17:14 +0300

    add yaml reader

--- delta-pyspark/profiler/config_collector.py
+++ delta-pyspark/profiler/config_collector.py
@@ -1,17 +1,16 @@
+import json
 import yaml
 
-class YamlConfigParser:
+class YamlConfigReader:
     def __init__(self, file_path: str):
         self.file_path = file_path
 
-    def read_yaml(self):
+    def parse_yaml_file(self):
         with open(self.file_path) as f:
             try:
                 profilerconfig = yaml.safe_load(f)
-                print(profilerconfig)
+                print(json.dumps(profilerconfig, indent=4))
+                return profilerconfig
             except yaml.YAMLError as e:
                 print(e)
-
-    def 
-
     
